15169199f08162327f4667eb98fd45deda44be83
Anna Varaksina <enigmasnape@gmail.com>
2024-01-08 00:32:38 +0300

    typescript and calendar added

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+import React from "react";
+import App from "./App";
+import { createRoot } from "react-dom/client";
+
+const domNode = document.getElementById("root") as HTMLElement;
+const root = createRoot(domNode);
+
+root.render(<App />);
