71b8858070cfb8ae3a3c1e1a5badd21cd598e62d
Anna Varaksina <enigmasnape@gmail.com>
2024-01-23 23:57:06 +0300

    ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,128 +1,119 @@
 import React, { useState } from "react";
+
+import MyCalendar from "./components/calendar/calendar";
 import "./index.css";
-import type { Dayjs } from "dayjs";
-import dayjs from "dayjs";
-import type { BadgeProps, CalendarProps } from "antd";
-import { Alert, Badge, Calendar, Modal } from "antd";
 
-const getListData = (value: Dayjs) => {
-  let listData;
-  switch (value.date()) {
-    case 8:
-      listData = [
-        { type: "warning", content: "This is warning event." },
-        { type: "success", content: "This is usual event." },
-      ];
-      break;
-    case 10:
-      listData = [
-        { type: "warning", content: "This is warning event." },
-        { type: "success", content: "This is usual event." },
-        { type: "error", content: "This is error event." },
-      ];
-      break;
-    case 15:
-      listData = [
-        { type: "warning", content: "This is warning event" },
-        { type: "success", content: "This is very long usual event......" },
-        { type: "error", content: "This is error event 1." },
-        { type: "error", content: "This is error event 2." },
-        { type: "error", content: "This is error event 3." },
-        { type: "error", content: "This is error event 4." },
-      ];
-      break;
-    default:
-  }
-  return listData || [];
-};
+// import type { Dayjs } from "dayjs";
+// import dayjs from "dayjs";
+// import type { BadgeProps, CalendarProps } from "antd";
+// import { Alert, Badge, Calendar, Modal } from "antd";
 
-const getMonthData = (value: Dayjs) => {
-  if (value.month() === 8) {
-    return 1394;
-  }
-};
+// const getListData = (value: Dayjs) => {
+//   let listData;
+//   switch (value.date()) {
+//     case 8:
+//       listData = [
+//         { type: "warning", content: "This is warning event." },
+//         { type: "success", content: "This is usual event." },
+//       ];
+//       break;
+//     case 10:
+//       listData = [
+//         { type: "warning", content: "This is warning event." },
+//         { type: "success", content: "This is usual event." },
+//         { type: "error", content: "This is error event." },
+//       ];
+//       break;
+//     case 15:
+//       listData = [
+//         { type: "warning", content: "This is warning event" },
+//         { type: "success", content: "This is very long usual event......" },
+//         { type: "error", content: "This is error event 1." },
+//         { type: "error", content: "This is error event 2." },
+//         { type: "error", content: "This is error event 3." },
+//         { type: "error", content: "This is error event 4." },
+//       ];
+//       break;
+//     default:
+//   }
+//   return listData || [];
+// };
 
-interface Event {
-  content: string;
-  type: string;
-  startTime?: Dayjs;
-  endTime?: Dayjs;
-  remindTime?: unknown; // todo
-}
+// const getMonthData = (value: Dayjs) => {
+//   if (value.month() === 8) {
+//     return 1394;
+//   }
+// };
 
-const eventsModal = (events: Event[]) => {
-  Modal.info({
-    title: "Events info",
-    content: (
-      <ul className="events-list">
-        {events.map((event: Event, index: number) => {
-          return <li key={index}>{event.content}</li>;
-        })}
-      </ul>
-    ),
-    onOk() {},
-  });
-};
+// interface Event {
+//   content: string;
+//   type: string;
+//   startTime?: Dayjs;
+//   endTime?: Dayjs;
+//   remindTime?: unknown; // todo
+// }
+
+// const eventsModal = (events: Event[]) => {
+//   Modal.info({
+//     title: "Events info",
+//     content: (
+//       <ul className="events-list">
+//         {events.map((event: Event, index: number) => {
+//           return <li key={index}>{event.content}</li>;
+//         })}
+//       </ul>
+//     ),
+//     onOk() {},
+//   });
+// };
 
 const App: React.FC = () => {
-  const [value, setValue] = useState(() => dayjs());
-  const [selectedValue, setSelectedValue] = useState(() => dayjs());
+  // const [value, setValue] = useState(() => dayjs());
+  // const [selectedValue, setSelectedValue] = useState(() => dayjs());
 
-  const onSelect = (newValue: Dayjs) => {
-    setValue(newValue);
-    setSelectedValue(newValue);
-    eventsModal(getListData(newValue));
-  };
+  // const onSelect = (newValue: Dayjs) => {
+  //   setValue(newValue);
+  //   setSelectedValue(newValue);
+  //   eventsModal(getListData(newValue));
+  // };
 
-  const onPanelChange = (newValue: Dayjs) => {
-    setValue(newValue);
-  };
+  // const onPanelChange = (newValue: Dayjs) => {
+  //   setValue(newValue);
+  // };
 
-  const monthCellRender = (value: Dayjs) => {
-    const num = getMonthData(value);
-    return num ? (
-      <div className="notes-month">
-        <section>{num}</section>
-        <span>Backlog number</span>
-      </div>
-    ) : null;
-  };
+  // const monthCellRender = (value: Dayjs) => {
+  //   const num = getMonthData(value);
+  //   return num ? (
+  //     <div className="notes-month">
+  //       <section>{num}</section>
+  //       <span>Backlog number</span>
+  //     </div>
+  //   ) : null;
+  // };
 
-  const dateCellRender = (value: Dayjs) => {
-    const listData = getListData(value);
-    return (
-      <ul className="events">
-        {listData.map((item) => (
-          <li key={item.content}>
-            <Badge
-              status={item.type as BadgeProps["status"]}
-              text={item.content}
-            />
-          </li>
-        ))}
-      </ul>
-    );
-  };
+  // const dateCellRender = (value: Dayjs) => {
+  //   const listData = getListData(value);
+  //   return (
+  //     <ul className="events">
+  //       {listData.map((item) => (
+  //         <li key={item.content}>
+  //           <Badge
+  //             status={item.type as BadgeProps["status"]}
+  //             text={item.content}
+  //           />
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   );
+  // };
 
-  const cellRender: CalendarProps<Dayjs>["cellRender"] = (current, info) => {
-    if (info.type === "date") return dateCellRender(current);
-    if (info.type === "month") return monthCellRender(current);
-    return info.originNode;
-  };
+  // const cellRender: CalendarProps<Dayjs>["cellRender"] = (current, info) => {
+  //   if (info.type === "date") return dateCellRender(current);
+  //   if (info.type === "month") return monthCellRender(current);
+  //   return info.originNode;
+  // };
 
-  return (
-    <>
-      <Alert
-        message={`You selected date: ${selectedValue?.format("YYYY-MM-DD")}`}
-      />
-      <Calendar
-        value={value}
-        cellRender={cellRender}
-        onSelect={onSelect}
-        onPanelChange={onPanelChange}
-      />
-    </>
-  );
+  return <MyCalendar />;
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import App from "./App";
+import store from "./store";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const domNode = document.getElementById("root") as HTMLElement;
 const root = createRoot(domNode);
